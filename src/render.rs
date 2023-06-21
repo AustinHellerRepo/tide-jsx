b84d1b3967e5292826890d6b96a6300c402d37d4
unknown <galaky2005@gmail.com>
2023-06-21 17:52:35 -0400

    added non-reference variant;

--- src/render.rs
+++ src/render.rs
@@ -28,6 +28,12 @@ impl Render for Box<&dyn Render> {
     }
 }
 
+impl Render for Box<dyn Render> {
+    fn render_into<W: Write>(self, writer: &mut W) -> Result where Self: Sized {
+        self.render_into(writer)
+    }
+}
+
 /// Does nothing
 impl Render for () {
     fn render_into<W: Write>(self, _writer: &mut W) -> Result {
