887b7b7bbdf496fd11aec496142eeaa48ed10358
unknown <galaky2005@gmail.com>
2023-06-29 08:23:01 -0400

    fixed raw! to accept Into &str instead of only &str;

--- src/text_element.rs
+++ src/text_element.rs
@@ -24,15 +24,9 @@ impl Render for std::borrow::Cow<'_, str> {
 #[derive(Debug)]
 pub struct Raw<'s>(&'s str);
 
-impl<'s> From<&'s str> for Raw<'s> {
-    fn from(s: &'s str) -> Self {
-        Raw(s)
-    }
-}
-
-impl<'s> From<&'s String> for Raw<'s> {
-    fn from(s: &'s String) -> Self {
-        Raw(s)
+impl<'s, T: Into<&'s str>> From<T> for Raw<'s> {
+    fn from(s: T) -> Self {
+        Raw(s.into())
     }
 }
 
